a133a130a485924aecf2af30326535b70b8b2599
Thiago Lopes <tiagojosecnn@outlook.com>
2024-07-14 21:04:51 -0300

    Adiciona camada controller.

---
+++ src/main/java/com/example/sghapi/api/controller/CategoriaController.java
@@ -0,0 +1,16 @@
+package com.example.sghapi.api.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("api/v1/categorias")
+@RequiredArgsConstructor
+@CrossOrigin
+public class CategoriaController {
+
+
+}
