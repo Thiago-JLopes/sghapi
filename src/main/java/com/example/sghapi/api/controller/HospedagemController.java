c93ada41cc3d02ad02d75a4a65e3710570365570
Alfredo Lucas <alfredoliu14@live.com>
2024-08-12 14:19:19 -0300

    Modifica controller de Hospedagem

--- src/main/java/com/example/sghapi/api/controller/HospedagemController.java
+++ src/main/java/com/example/sghapi/api/controller/HospedagemController.java
@@ -2,11 +2,17 @@ package com.example.sghapi.api.controller;
 
 import com.example.sghapi.api.dto.ClienteDTO;
 import com.example.sghapi.api.dto.HospedagemDTO;
+import com.example.sghapi.exception.RegraNegocioException;
 import com.example.sghapi.model.entity.Cliente;
+import com.example.sghapi.model.entity.Funcionario;
 import com.example.sghapi.model.entity.Hospedagem;
+import com.example.sghapi.model.entity.Reserva;
 import com.example.sghapi.service.ClienteService;
+import com.example.sghapi.service.FuncionarioService;
 import com.example.sghapi.service.HospedagemService;
+import com.example.sghapi.service.ReservaService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +27,8 @@ import java.util.stream.Collectors;
 @CrossOrigin
 public class HospedagemController {
     private final HospedagemService service;
+    private final FuncionarioService funcionarioService;
+    private final ReservaService reservaService;
 
     @GetMapping()
     public ResponseEntity get() {
@@ -35,5 +43,65 @@ public class HospedagemController {
             return new ResponseEntity("hospedagem não encontrado", HttpStatus.NOT_FOUND);
         }
         return ResponseEntity.ok(hospedagem.map(HospedagemDTO::create));
+    }
+
+    @PostMapping()
+    public ResponseEntity post(HospedagemDTO dto){
+        try{
+            Hospedagem hospedagem = converter(dto);
+            Reserva reserva = reservaService.salvar(hospedagem.getReserva());
+            hospedagem.setReserva(reserva);
+            hospedagem = service.salvar(hospedagem);
+            return new ResponseEntity(hospedagem, HttpStatus.CREATED);
+        }catch (RegraNegocioException e){
+            return  ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity atualizar(@PathVariable("id") Long id, HospedagemDTO dto){
+        if(!service.getHospedagemById(id).isPresent()){
+            return new ResponseEntity("Hospedagem não encontrada", HttpStatus.NOT_FOUND);
+        }
+        try{
+            Hospedagem hospedagem = converter(dto);
+            hospedagem.setId(id);
+            Reserva reserva = reservaService.salvar(hospedagem.getReserva());
+            hospedagem.setReserva(reserva);
+            service.salvar(hospedagem);
+            return ResponseEntity.ok(hospedagem);
+        }catch (RegraNegocioException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity excluir(@PathVariable("id") Long id){
+        Optional<Hospedagem> hospedagem = service.getHospedagemById(id);
+        if(!hospedagem.isPresent()){
+            return new ResponseEntity("Hospedagem não encontrada", HttpStatus.NOT_FOUND);
+        }
+        try{
+            service.excluir(hospedagem.get());
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        }catch(RegraNegocioException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    public Hospedagem converter(HospedagemDTO dto){
+        ModelMapper modelMapper = new ModelMapper();
+        Hospedagem hospedagem = modelMapper.map(dto, Hospedagem.class);
+        Reserva reserva = modelMapper.map(dto, Reserva.class);
+        hospedagem.setReserva(reserva);
+        if(dto.getIdFuncionario() != null){
+            Optional<Funcionario> funcionario = funcionarioService.getFuncionarioById(dto.getIdFuncionario());
+            if(!funcionario.isPresent()){
+                hospedagem.setFuncionario(null);
+            }else{
+                hospedagem.setFuncionario(funcionario.get());
+            }
+        }
+        return  hospedagem;
     }
 }
