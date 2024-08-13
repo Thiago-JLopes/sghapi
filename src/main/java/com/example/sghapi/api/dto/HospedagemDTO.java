36eca275f94b7b6022dc3435710e888d8fd969f5
Thiago Lopes <tiagojosecnn@outlook.com>
2024-08-13 16:28:04 -0300

    adiciona anotação lombok.

--- src/main/java/com/example/sghapi/api/dto/HospedagemDTO.java
+++ src/main/java/com/example/sghapi/api/dto/HospedagemDTO.java
@@ -2,15 +2,21 @@ package com.example.sghapi.api.dto;
 
 import com.example.sghapi.model.entity.Funcionario;
 import com.example.sghapi.model.entity.Hospedagem;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.modelmapper.ModelMapper;
 
 import java.time.LocalDateTime;
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class HospedagemDTO {
     private Long id;
     private LocalDateTime CheckInReal;
     private LocalDateTime CheckOutReal;
     private Float gastos;
+    private Long idFuncionario;
 
     public static HospedagemDTO create(Hospedagem hospedagem) {
         ModelMapper modelMapper = new ModelMapper();
